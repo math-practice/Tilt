3a8e2ba1a2337f88e849a764a48e3e174530278d
Nico <jnico.chilla@gmail.com>
2023-01-22 18:19:02 -0500

    no more jquery

--- assets/script.js
+++ assets/script.js
@@ -22,117 +22,110 @@ var maxRotation = 45;
 var minRotation = -45;
 var hrot, vrot, transformW, transformH;
 const isMobile=window.matchMedia('(hover:none)');
+const phoneLayout=window.matchMedia('(max-width:599px)');
 
 let trackingStarted = false;
 
 let scrollerHeight=window.matchMedia('(max-width:599px)').matches?129:240;
 let scrollerLetters=[];
 
+document.querySelectorAll('.sign').forEach((sign, s) => {
+  sign.addEventListener('mousemove',function(){
+    var x = event.pageX - sign.offsetLeft;
+    var y = event.pageY - sign.offsetTop;
+    var w = sign.offsetWidth;
+    var h = sign.offsetHeight;
+    hrot = (x / w * maxRotation * 2 - maxRotation) * -1;
+    vrot = (y / h * maxRotation * 2 - maxRotation) * -1;
+    transformW = (100 - Math.abs(hrot)) / 100;
+    transformH = (100 - Math.abs(vrot)) / 100;
+    tiltSign(sign)
+  })
 
+  sign.addEventListener('mouseleave',function(){
+    sign.querySelectorAll(".inner p, .arc p").forEach((item) => {
+      item.style.fontVariationSettings="'HROT' 0, 'VROT' 0";
+      item.style.width='';
+      item.style.height='';
+    });
+    sign.querySelectorAll(".reactive").forEach((item) => {
+      item.style.width="100%";
+    });
 
-$(".sign").mousemove(function(event){
-  var x = event.pageX - $(this).offset().left;
-  var y = event.pageY - $(this).offset().top;
-  var w = $(this).width();
-  var h = $(this).height();
-  hrot = (x / w * maxRotation * 2 - maxRotation) * -1;
-  vrot = (y / h * maxRotation * 2 - maxRotation) * -1;
-  transformW = (100 - Math.abs(hrot)) / 100;
-  transformH = (100 - Math.abs(vrot)) / 100;
-  // console.log (hrot + ", " + vrot);
-
-  tiltSign(this)
-
-
-
+  })
 });
+
+
 
 
 function tiltSign(sign){
 
-  $(sign).find(".inner p").css({
-    fontVariationSettings: "'HROT' " + hrot + ", 'VROT' " + vrot,
-    // lineHeight: 105 * transformH + "%"
+
+  sign.querySelectorAll(".inner p").forEach((item) => {
+    item.style.fontVariationSettings= "'HROT' " + hrot + ", 'VROT' " + vrot;
   });
 
-  $(sign).find(".reactive").css({
-    width: 100-Math.abs(hrot) + "%",
-    // height: 100-Math.abs(vrot) + "%",
-  })
 
-  $(sign).find(".arc p span").each(function(){
-    // rotation angle of this letter
-    var r = getRotationDegrees($(sign));
-
-    //r * pi / 180 to return sine wave, then shift the r 90 degrees to get the wave to start from 1 (not 0)
-    //https://docs.google.com/spreadsheets/d/1L3u8t-3eQaisRTdMZ-T2UWpbDCKLc8AIePK4wCZYsFE/edit#gid=0
-
-    var sinShift = Math.sin( r * Math.PI / 180);
-    var sin = Math.sin( (r + 90) % 360 * Math.PI / 180);
-
-    //calc initial rotations
-    var newHrot = sin * hrot + sinShift * vrot;
-    var newVrot = sin * vrot + sinShift * hrot;
-
-    if ($(sign).attr('class') == 'arc-1') {
-      console.log ("HROT: " + hrot + " VROT: " + vrot + " NEW HROT:" + newHrot);
-    }
-
-    $(sign).css({
-      fontVariationSettings: "'HROT' " + hrot + ", 'VROT' " + vrot,
-    });
+  sign.querySelectorAll(".reactive").forEach((item) => {
+    item.style.width= 100-Math.abs(hrot) + "%";
   });
 
-  //if box
-  svgTransform($(sign).find(".box"), 33);
+
+
+  // var r = getRotationDegrees(sign);
+  // //r * pi / 180 to return sine wave, then shift the r 90 degrees to get the wave to start from 1 (not 0)
+  // //https://docs.google.com/spreadsheets/d/1L3u8t-3eQaisRTdMZ-T2UWpbDCKLc8AIePK4wCZYsFE/edit#gid=0
+  // var sinShift = Math.sin( r * Math.PI / 180);
+  // var sin = Math.sin( (r + 90) % 360 * Math.PI / 180);
+  // var newHrot = sin * hrot + sinShift * vrot;
+  // var newVrot = sin * vrot + sinShift * hrot;
+
+  sign.style.fontVariationSettings="'HROT' " + hrot + ", 'VROT' " + vrot;
+
+  sign.querySelectorAll('.box').forEach((box) => {
+    box.style.width=33 * transformW + "vw";
+  });
+
 }
 
 
-$(".sign").mouseleave(function(event){
-  $(this).find(".inner p, .arc p").css({
-    fontVariationSettings: "'HROT' 0, 'VROT' 0",
-    width: "",
-    height: ""
-  });
-  $(this).find(".reactive").css({
-    width: "100%",
-    // height: "100%"
-  });
 
-});
 
-function svgTransform(svg, svgWidth){
-  if (svg.length) {
-    svg.css ({
-      width: svgWidth * transformW + "vw"
-    });
+function getRotationDegrees(el) {
+  // https://stackoverflow.com/questions/19574171/how-to-get-css-transform-rotation-value-in-degrees-with-javascript/54492696#54492696
+
+  var st = window.getComputedStyle(el, null);
+  var tm = st.getPropertyValue("-webkit-transform") ||
+           st.getPropertyValue("-moz-transform") ||
+           st.getPropertyValue("-ms-transform") ||
+           st.getPropertyValue("-o-transform") ||
+           st.getPropertyValue("transform") ||
+           "none";
+  if (tm != "none") {
+    var values = tm.split('(')[1].split(')')[0].split(',');
+    var angle = Math.round(Math.atan2(values[1],values[0]) * (180/Math.PI));
+    return (angle < 0 ? angle + 360 : angle); //adding 360 degrees here when angle < 0 is equivalent to adding (2 * Math.PI) radians before
   }
-  // console.log ("[" + svgWidth + ", " + svgHeight + "] " + transformW + ", " + transformH);
-}
+  return 0;
 
-function getRotationDegrees(obj) {
-  //https://stackoverflow.com/questions/8270612/get-element-moz-transformrotate-value-in-jquery
-  var matrix = obj.css("-webkit-transform") ||
-  obj.css("-moz-transform")    ||
-  obj.css("-ms-transform")     ||
-  obj.css("-o-transform")      ||
-  obj.css("transform");
-  if(matrix !== 'none') {
-      var values = matrix.split('(')[1].split(')')[0].split(',');
-      var a = values[0];
-      var b = values[1];
-      var angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
-  } else { var angle = 0; }
-  return (angle < 0) ? angle + 360 : angle;
 }
 
 /* ------------------------------------------------------------
   Other stuff
 ------------------------------------------------------------ */
-$('.open-glyphs-table').click(function(event){
-  event.preventDefault();
-  $(this).closest('section').next(".glyphs-table").slideToggle();
-  $(this).closest('section').next(".glyphs-table").get(0).scrollIntoView({behavior:'smooth'});
+
+document.querySelectorAll('.open-glyphs-table').forEach((button) => {
+  let table=document.querySelector(`.glyphs-table[data-id="${button.dataset.id}"]`);
+  // table.
+
+  // 37.5vw height on desktop
+  //150vw on mobile
+
+
+  button.addEventListener('click',function(){
+    table.classList.toggle('open');
+    table.scrollIntoView({behavior:'smooth'});
+  })
 });
 
 
@@ -306,39 +299,6 @@ document.querySelectorAll('.scroller').forEach((section,i) => {
       }else{
         entry.target.style.fontVariationSettings=`"HROT" -45, "VROT" 0`;
       }
-      // let xRatio=entry.intersectionRect.width/entry.boundingClientRect.width;
-      // if(xRatio>0&&xRatio<1&&entry.intersectionRect.x>0){
-      //   console.log(xRatio)
-      //   entry.target.style.fontVariationSettings=`"HROT" 0, "VROT" 0`;
-      // }else{
-      //
-      //   entry.target.style.fontVariationSettings=`"HROT" -45, "VROT" 0`;
-      // }
-
-
-      // if(entry.boundingClientRect.left<=section.offsetWidth/2){
-      //   entry.target.style.fontVariationSettings=`"HROT" -45, "VROT" 0`;
-      // }else{
-      //   console.log(entry);
-      //   entry.target.style.fontVariationSettings=`"HROT" 45, "VROT" 0`;
-      // }
-
-      // else if(entry.intersectionRect.x>0&&entry.intersectionRatio<1){
-      //   console.log(entry.intersectionRatio)
-      //   entry.target.style.fontVariationSettings=`"HROT" 0, "VROT" 0`;
-      // }else{
-      //   entry.target.style.fontVariationSettings=`"HROT" 45, "VROT" 0`;
-      // }
-
-
-
-      // if(entry.intersectionRect.x>0){
-      //   if(entry.intersectionRatio<1){
-      //
-      //   }
-      // }else{
-      //
-      // }
 
     }
   }
